b1b47cbafc15531714a2a5d7d50cb1176a32cded
Rich Cordero <rich@corderophilosophy.com>
2016-06-20 20:29:25 -0400

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // webpack folder's entry js - excluded from jekyll's build process.
+  entry: "./webpack/entry.js",
+  output: {
+    // put generated file in assets folder so jekyll will grab it
+    path: 'src/assets/js/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
+};
---
+++ webpack/components/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render () {
+    return (
+      <div> Hey, y'all!</div>
+    )
+  }
+}
+
+export default Hello;
